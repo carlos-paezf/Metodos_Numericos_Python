fb557dde593c6a690990e387a4885375e6dd7101
carlos-paezf <carlos.paezf@usantoto.edu.co>
2020-08-04 15:12:52 -0500

    Series Taylor para el Seno

---
+++ Series_Taylor_Seno.py
@@ -0,0 +1,27 @@
+import math
+
+#	Convertir argumento a radianes
+x_deg = float(input('Valor del argumento en grados: '))
+x = math.radians(x_deg)
+
+n = int(input('Número de terminos a sumar: '))
+
+#	Error verdadadero/total porcentual
+err_verd = 100.0
+
+#	Valor verdadero de sen(x)
+sen_verd = math.sin(x)
+
+#	Inicializar el acumulador en 0
+sen_x = 0.0
+
+#	Encabezados
+print ("\n\n{:^2} {:^10} {:^16} {:^20}".format("Termino", "k", "sen(x)", "err_verd"))
+
+#	Acumular/Sumar terminos
+for k in range(n):
+	#	Acumula la suma
+	sen_x = sen_x + (-1)**k * x**(2*k+1) / math.factorial((2*k+1))
+	#	Calcula el error verdadero
+	err_verd = abs((sen_verd -sen_x) / sen_verd)*100
+	print("{:^2d} {:^19d} {:^15.10f} {:^15.4g}".format(k+1, k, sen_x, err_verd))
